5b408b9fb461f092acf4e75dea8cedee5bb34abe
IvayloZashev <ivaylo_zashev@yahoo.bg>
2015-07-16 16:44:43 +0300

    Change parent library

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,12 @@
 package calculator;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootApplication
 @ComponentScan
-@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
--- src/main/java/calculator/FiguresController.java
+++ src/main/java/calculator/FiguresController.java
@@ -21,54 +21,33 @@ public class FiguresController {
     FiguresService figuresService;
 
     @RequestMapping(value = "/sum", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    ResponseEntity<SumEntity> sum(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
-
+    @ResponseBody public ResponseEntity<SumEntity> sum(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
         int result = figuresService.sum(first, second);
-
         SumEntity sum = new SumEntity(counter.incrementAndGet(), result);
-
         return new ResponseEntity<>(sum, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/subtract", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    ResponseEntity<SubtractEntity> subtract(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
-
+    @ResponseBody public ResponseEntity<SubtractEntity> subtract(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
         int result = figuresService.difference(first, second);
-
         SubtractEntity subtract = new SubtractEntity(counter.incrementAndGet(), result);
-
         return new ResponseEntity<>(subtract, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/multiply", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    ResponseEntity<MultiplyEntity> multiply(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
-
+    @ResponseBody public ResponseEntity<MultiplyEntity> multiply(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
         int result = figuresService.multiply(first, second);
-
         MultiplyEntity multiply = new MultiplyEntity(counter.incrementAndGet(), result);
-
         return new ResponseEntity<>(multiply, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public
     @ResponseBody
-    ResponseEntity<Figures> allOperations(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
-
+    public ResponseEntity<Figures> allOperations(@RequestParam(value = "first", defaultValue = "0") int first, @RequestParam(value = "second", defaultValue = "0") int second) {
         int sum = figuresService.sum(first, second);
-
         int subtract = figuresService.difference(first, second);
-
         int multiply = figuresService.multiply(first, second);
-
         Figures figures = new Figures(counter.incrementAndGet(), first, second, sum, subtract, multiply);
-
         return new ResponseEntity<>(figures, HttpStatus.OK);
     }
 }
